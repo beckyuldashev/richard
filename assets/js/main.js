5277838264aa557b6ecaaa26a6f12ccb2e50e3b1
Beck Yuldashev <yuldashev.bek@gmail.com>
2022-07-22 13:43:06 +0300

    Added scripts to change header bg and media queries

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,3 +5,32 @@ const swiper = new Swiper('.swiper', {
     el: '.swiper-pagination',
   }
 });
+
+
+
+// Change header bg
+function scrollHeader() {
+  const header = document.querySelector('.header');
+
+  if(this.scrollY > 50) {
+    header.classList.add('scroll-header');
+  } else {
+    header.classList.remove('scroll-header');
+  }
+}
+
+window.addEventListener('scroll', scrollHeader);
+
+
+// Show scroll up
+function showScrollUp() {
+  const scrollUp = document.querySelector('.scroll-up');
+
+  if(this.scrollY > 200) {
+    scrollUp.classList.add('show-scroll-up');
+  } else {
+    scrollUp.classList.remove('show-scroll-up');
+  }
+}
+
+window.addEventListener('scroll', showScrollUp);
